cbf45910c0bc81888a62620d98de4c4b08b3b619
Matthew Jones <mattjones1990@hotmail.co.uk>
2023-01-21 14:51:35 +0000

    first commit

---
+++ BasicCLIProgram.go
@@ -0,0 +1,45 @@
+// package main
+
+// import (
+// 	"bufio"
+// 	"flag"
+// 	"fmt"
+// 	"log"
+// 	"os"
+// 	"strings"
+// )
+
+// func main() {
+// 	path := flag.String("path", "myapp.log", "The path to the log that should be analysed")
+// 	level := flag.String("level", "ERROR", "Log level to search for. Options are DEBUG, INFO and ERROR")
+
+// 	flag.Parse()
+
+// 	f, err := os.Open(*path)
+
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	defer f.Close()
+
+// 	r := bufio.NewReader(f)
+
+// 	for {
+// 		s, err := r.ReadString('\n')
+
+// 		if err != nil {
+// 			break
+// 		}
+
+// 		if strings.Contains(s, *level) {
+// 			fmt.Println(s)
+// 		}
+
+// 	}
+// }
+
+//go run BasicCLIProgram.go
+//go run BasicCLIProgram.go -level DEBUG
+//go run BasicCLIProgram.go -level DEBUG -path test.log
+//go run BasicCLIProgram.go -level DEBUG -path myapp.log
