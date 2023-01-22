cb8240e1106ac79b17a7331c3002f26bfe4ab3d9
Matthew Jones <mattjones1990@hotmail.co.uk>
2023-01-22 08:05:48 +0000

    update constants

--- Constants.go
+++ Constants.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+const (
+	numberOne   = iota //0
+	numberTwo   = iota //1
+	numberThree = iota //2
+
+	first  = "one"
+	second = "second"
+
+	numberFour = iota //will by 5 as there's two other constants in the way ^
+	numberFive        //assumes iota from above = 6
+
+)
+
+const (
+	numberSix = iota //resets to zero
+)
+
 func main() {
 	const pi = 3.1415 //must be initialised at the same time
 	fmt.Println(pi)
@@ -17,4 +34,7 @@ func main() {
 	fmt.Println(d + 3)
 
 	fmt.Println(float32(d) + 1.2)
+
+	fmt.Println(first, second)
+	fmt.Println(numberOne, numberTwo, numberThree, numberFour, numberFive, numberSix)
 }
